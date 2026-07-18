7c8f1abd428cbac0df826c6cb437f731a3b3cccf
aww-sam <guragaisamyog30@gmail.com>
2026-07-18 22:55:38 +0545

    corrected_logic

--- LAB4/floyd_warsall.py
+++ LAB4/floyd_warsall.py
@@ -1,30 +1,25 @@
-def Floyd_warsall(w,n):
-    D=[]
-    for i in range(n+1):
-        row=[]
-        for j in range(n+1):
-            row.append(w[i][j])
-        D.append(row)
-    
-    for k in range(1,n+1):
-        for i in range(1,n+1):
-            for j in range(1,n+1):
-                if D[i][k]+ D[k][j] < D[i][j]:
-                    D[i][j]=D[i][k]+D[k][j]
+def Floyd_warsall(w, n):
+    D = [row[:] for row in w]
+
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if D[i][k] + D[k][j] < D[i][j]:
+                    D[i][j] = D[i][k] + D[k][j]
     return D
 
-INF=float('inf')
-n=4
-W=[
-    [INF,INF,INF,INF,INF],
-    [INF,0,INF,3,INF],
-    [INF,2,0,INF,INF],
-    [INF,INF,7,0,1],
-    [INF,6,INF,INF,0]
+
+INF = float('inf')
+n = 4
+W = [
+    [0, INF, 3, INF],
+    [2, 0, INF, INF],
+    [INF, 7, 0, 1],
+    [6, INF, INF, 0]
 ]
 
-d=Floyd_warsall(W,n)
-for i in range(1,n+1):
-    for j in range(1,n+1):
-        print(d[i][j],end='\t')
+d = Floyd_warsall(W, n)
+for i in range(n):
+    for j in range(n):
+        print(d[i][j], end='\t')
     print()
